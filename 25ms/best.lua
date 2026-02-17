8d34f18f14ffa5b2151fea7d0ed5bab3859932e0
khen <chakhenne@gmail.com>
2026-02-17 15:13:59 +0700

    Create best.lua

---
+++ 25ms/best.lua
@@ -0,0 +1,7 @@
+-- ts file was generated at discord.gg/25ms
+
+local fenv = getfenv()
+local _ = fenv.iYXCBMuhM1jKU
+local _ = getmetatable(_G).__newindex
+
+error('line 1: dtc, should join discord.gg/quMHYgkRfD. #1685')
