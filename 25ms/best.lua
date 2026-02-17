2ddc701650e1e151a33a8890826cbc14be86a319
khen <chakhenne@gmail.com>
2026-02-17 15:21:57 +0700

    Update best.lua

--- 25ms/best.lua
+++ 25ms/best.lua
@@ -1,7 +1,13 @@
 -- ts file was generated at discord.gg/25ms
 
+local genv = getgenv()
 local fenv = getfenv()
-local _ = fenv.iYXCBMuhM1jKU
-local _ = getmetatable(_G).__newindex
 
-error('line 1: dtc, should join discord.gg/quMHYgkRfD. #1685')
+genv['khen was here'] = function(_2, _2_2, _2_3, _2_4)
+    local _ = fenv.BgVaTjcGmQUl
+end
+
+local _ = fenv.Ga3L2zKmfaOcwv
+
+type(game:GetService('Players').LocalPlayer)
+error('line 1: dtc, should join discord.gg/quMHYgkRfD. #89674')
