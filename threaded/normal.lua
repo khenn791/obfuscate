ad4b3eb5c0eec584d2277f31a4531c5e64da530c
khen <chakhenne@gmail.com>
2026-02-17 15:06:21 +0700

    Create normal.lua

---
+++ threaded/normal.lua
@@ -0,0 +1,14 @@
+-- This file was generated with UnveilR V3 at discord.gg/threaded using the testing version.
+
+local fenv = getfenv();
+local success = pcall(function(p1, p2, p3, p4, p5, p6, p7, p8, p9, a, b, c)
+    getgenv()["khen was here"] = function(p1_0, p2_0, p3_0, p4_0, a_0, b_0, c_0) end;
+    local _ = fenv.Ga3L2zKmfaOcwv;
+end);
+local _ = game.GetService;
+local v2 = game:GetService("Players");
+local LocalPlayer = v2.LocalPlayer;
+local v4 = not LocalPlayer;
+local v5 = typeof(LocalPlayer);
+local v6 = LocalPlayer.ClassName;
+error"dtc, should join (discord invite). #89674";
