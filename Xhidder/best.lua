2800cdc12101bb6a7567ed818b2f0c1bf076b168
khen <chakhenne@gmail.com>
2026-02-19 19:51:58 +0700

    Update best.lua

--- Xhidder/best.lua
+++ Xhidder/best.lua
@@ -1,3 +1,5 @@
 --// This file was created by XHider https://discord.com/invite/E2N7w35zkt
 
-getgenv().['khen was here'] = function(p1, p2, p3, p4, p5, p6) -- upval _1: function: 00000000006a03d0; -- upval _0: 14506955; -- upval _2: table: 000000000062e180; -- upval _3: table: 000000000062ecc0 end
+getgenv().khen was here = function() end
+local _v1 = require("lune")
+local _v = task.wait(0)
