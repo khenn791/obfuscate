cc445eb0142474d7777a5325e40832abde848483
khen <chakhenne@gmail.com>
2026-02-19 16:14:59 +0700

    Update best.lua

--- Phantoraph/best.lua
+++ Phantoraph/best.lua
@@ -1,9 +1,2 @@
 -- this file is generated using lunr discord.gg/9yAtRgpsua
-
-local Service = game:GetService("Service")
-local LocalPlayer = Service.LocalPlayer
-local Part = Instance.new("Part")
-Part:Destroy()
-local Part1 = Instance.new("Part")
-Part1:Destroy()
 -- Terminated: Error ([string "return (function(v0,v2,v3,v4,v5,v6,v7,v14,v18..."]:1: dtc, should join discord.gg/quMHYgkRfD. #60978)
