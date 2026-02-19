d4495bbdfba9d12074a638a7f74b2a433428e47d
khen <chakhenne@gmail.com>
2026-02-19 16:08:49 +0700

    Update best.lua

--- 25ms/best.lua
+++ 25ms/best.lua
@@ -3,11 +3,13 @@
 local genv = getgenv()
 local fenv = getfenv()
 
-genv['khen was here'] = function(_2, _2_2, _2_3, _2_4)
-    local _ = fenv.BgVaTjcGmQUl
+genv['khen was here'] = function(_2, _2_2, _2_3, _2_4, _2_5, _2_6)
+    local _ = fenv.m0F0QxEYhcTFwM
 end
 
-local _ = fenv.Ga3L2zKmfaOcwv
+local _ = fenv.ycSre8AVug2kIb
+local _ = fenv.omdqUsb59uuou
+local _ = getmetatable(_G).__newindex
+local _ = fenv.eOPcTP6V8r1vZn
 
-type(game:GetService('Players').LocalPlayer)
-error('line 1: dtc, should join discord.gg/quMHYgkRfD. #89674')
+return 'dtc, should join discord.gg/quMHYgkRfD. #24648'
