2067bbf210457594bd59c49a90ddab3bb3cd1f3a
khen <chakhenne@gmail.com>
2026-02-19 16:13:10 +0700

    Update best.lua

--- Crypta/best.lua
+++ Crypta/best.lua
@@ -1,10 +1,2 @@
 -- [CRYPTA] this script was environment logged @ discord.gg/rY4TYaEPNY
-getgenv().khen was here = function: 0x000055ccd42cb370
-local Players1 = game:GetService("Players")
-./input.lua:907: dtc, should join discord.gg/quMHYgkRfD. #89674
-stacktrace:
-[C] function error
-./input.lua:907
-./input.lua:907
-./input.lua:907
-./input.lua:907
+getgenv().khen was here = function: 0x000055fb658f4140
