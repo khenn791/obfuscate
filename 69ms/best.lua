84046ba6d184ca022d3bed6921812b82d5348ce2
khen <chakhenne@gmail.com>
2026-02-22 10:25:00 +0700

    Create best.lua

---
+++ 69ms/best.lua
@@ -0,0 +1,10 @@
+--         __           .__         ___.                        
+--   ____ |  | ______   |__| ______ \_ |__   ____  ______ ______
+-- _/ __ \|  |/ /  _ \  |  |/  ___/  | __ \ /  _ \/  ___//  ___/
+-- \  ___/|    <  <_> ) |  |\___ \   | \_\ (  <_> )___ \ \___ \ 
+--  \___  >__|_ \____/  |__/____  >  |___  /\____/____  >____  >
+--      \/     \/               \/       \/           \/     \/ 
+-- join the discord to dump scripts https://discord.gg/yRrH6vHBCK
+
+getgenv().['khen was here'] = function() end
+-- -- VM terminated safely: [string "Obfuscated_Script"]:1: dtc, you should join .gg/KkmufKZ4R 77463
